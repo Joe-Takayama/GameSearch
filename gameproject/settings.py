d845764d046e3e37ee4c8e76c206809f1173ef7e
Joe-Takayama <jgaoshan549@gmail.com>
2026-02-20 12:44:37 +0900

    修正

--- gameproject/settings.py
+++ gameproject/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    "django.contrib.staticfiles",
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
